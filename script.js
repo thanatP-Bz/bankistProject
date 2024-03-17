f41c514f5507acda9b28004b1adf11c4e2b1f1f1
Thanat <bztzn512@gmail.com>
2024-03-17 09:56:13 -0400

    addedDeleteAccount

--- script.js
+++ script.js
@@ -171,6 +171,25 @@ btnTransfer.addEventListener("click", (event) => {
   updateUI(currentAccount);
 });
 
+btnClose.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+
+    accounts.splice(index, 1);
+
+    containerApp.style.opacity = 0;
+
+    inputClosePin.value = "";
+    inputCloseUsername.value = "";
+  }
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
